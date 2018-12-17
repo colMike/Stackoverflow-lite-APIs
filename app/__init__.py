219e5746f33b5ad4e3fe03be21c3f4c4093c2bfc
Michael Mbugua <MichaelNgigi76@gmail.com>
2018-12-17 08:22:27 +0300

    [starts #162692200] create endpoint to post a question

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,37 @@
-'''Creating app'''
-from flask import Flask
-from instance.config import app_config
-"""importing the configurations from the .config file which is in the instance folder"""
+from flask import Flask, jsonify, request
+import flask
+app = Flask(__name__)
 
-def create_app(config_name):
-    '''creating the app using the configurations in the dictionary created in the .config file'''
-
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_pyfile('config.py')
-    
-    return app
+questions = [
+            {'title': 'Title 1', 'body': 'Body 1', 'category': 'Category 1'}, 
+            {'title': 'Title 2', 'body': 'Body 2', 'category': 'Category 2'}, 
+            {'title': 'Title 3', 'body': 'Body 3', 'category': 'Category 3'}
+        ]
 
 
+
+# List all questions
+@app.route("/questions", methods=['GET'])
+def returnAll():
+    return jsonify({"questions": questions})
+
+
+# post a question
+@app.route("/add", methods=['POST'])
+def addOne():
+    title= request.get_json()['title']
+    body= request.get_json()['body']
+    category= request.get_json()['category']
+
+    question = {
+	    "title": title, 
+        "body": body,
+        "category": category
+   }
+
+    questions.append(question)
+    return jsonify({'questions' : questions}) 
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
